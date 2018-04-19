0da631752cf1665ca4ce2a027f6e8aa11f86a796
Helen <helentdougherty@gmail.com>
2018-04-19 09:39:22 -0400

    testing and i think we're a day late here

---
+++ getlight.py
@@ -0,0 +1,74 @@
+import serial
+import paho.mqtt.client as mqtt
+import string
+import sys
+
+import time
+import datetime
+import json
+import binascii
+import base64
+
+# python script that counts to 10 by passing a value between the xDot and the pivot server
+
+# there are two 'clients' - one is the xDot and one is the python script (via an MQTT client)
+
+# global variables
+network = "pivot.iuiot.org"
+
+
+pl1 = "{\"data\":\""
+pl2 = "\",\"deveui\":\"00-80-00-00-04-00-51-87\"}"
+
+
+pivread = True
+
+#pivot client stuff
+def on_message(client, userdata, msg):
+    pl = ""
+    # for s in msg.payload:
+    #     pl += binascii.b2a_qp(s)
+    # dj = json.dumps(pl)
+    # js = json.load(dj)
+    info = base64.b64decode(json.loads(msg.payload)['data'])
+    print('info = ' + info)
+
+    # need to convert the hex into floats
+    # get 96 hex numbers: each set of 4 is a float number
+    #also need to check the endianness
+
+    #dump = open("./light.txt",mode='a+')
+    #tm = time.ctime()
+    #dump.write('\n' + tm)
+
+    #print('temp  = ' + str(t))
+    #print('pressure = ' + str(p))
+    #dump.close()
+
+
+pivot = mqtt.Client()
+pivot.on_message = on_message
+pivot.connect(network)
+pivot.subscribe("lora/00-80-00-00-04-00-51-87/up")
+#pivot.loop_forever()
+
+def test():
+    ser = serial.Serial('/dev/ttyACM0', 115200)
+    print(ser.name)
+    pivot.loop_start()
+    while 1:
+        print('testing...')
+        time.sleep(2)
+        ser.write('s')
+    print('something is very wrong')
+
+def measure():
+    #ser = serial.Serial('/dev/ttyACM0', 115200)
+    #print(ser.name)
+    pivot.loop_start()
+    hrs = 0
+    while hrs < 49:
+        print('measuring data')
+        time.sleep(3600)
+    print('measuring finished')
+    pivot.loop_stop()
